c87a58d1edba604695acaf194550a8f0e5282869
Kwasi48 <abuwestwood@gmail.com>
2024-12-22 21:08:02 +0000

    this C stuff is hard for real

---
+++ file_copy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* copy input to output; 2nd version*/
+
+int main(int argc, char const *argv[])
+{
+    
+//     int c; 
+
+//    while((c = getchar()) != EOF)
+//        putchar(c);
+
+
+    int end;
+
+    while((end = getchar()) != EOF)
+         printf("%d\n", EOF);
+}
